2047a0ef1adfde0d4de845d81b391103d074b6e6
Rageh97 <abdelhady412rageh@gmail.com>
2023-07-23 23:57:08 +0300

    Add structure and dependencies

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+  return <div>page dashboard (protected)</div>;
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+  return <div>landing page (unprotected)</div>;
+};
+
+export default LandingPage;
